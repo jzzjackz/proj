99fdf41682fc1c9b3916627ce85675c1d079b5ae
jzzjackz <160585544+jzzjackz@users.noreply.github.com>
2025-06-13 21:49:15 +0200

    day-night cycle added
    i added a day-night cycle to the game, making it so every 5 minutes it switches to the opposite time

--- main.gd
+++ main.gd
@@ -3,9 +3,27 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("hi")
+	print("Download:", $Download)
+	print("Background:", $Background)
+	cycle()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func cycle() -> void:
+	while true:
+		var r1 = randi_range(60, 120)
+		$Day.show()
+		$Night.hide()
+		print("its day")
+		print(r1)
+		await get_tree().create_timer(r1).timeout
+		
+		r1 = randi_range(60, 120)
+		$Day.hide()
+		$Night.show()
+		print("its night")
+		print(r1)
+		await get_tree().create_timer(r1).timeout
